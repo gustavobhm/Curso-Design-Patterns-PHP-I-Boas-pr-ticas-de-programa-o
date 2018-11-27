382744c6f4604952d86864b83a7398d97d683269
Gustavo Luis dos Santos <gustavobhm@bol.com.br>
2018-11-27 08:49:52 -0200

    Aula 6 - Estados que variam e o State

--- exercicioConta/EstadoDaConta.php
+++
@@ -1,5 +0,0 @@
-<?php
-interface EstadoDaConta {
-  public function saca(Conta $conta, $valor);
-  public function deposita(Conta $conta, $valor);
-}
--- index.php
+++ index.php
@@ -1,41 +1,10 @@
 <?php
-	require "Item.php";
-	require "Desconto.php";
-	require "DescontoPorVendaCasada.php";
-	require "DescontoPorCincoItens.php";
-	require "DescontoPorMaisDeQuinhentosReais.php";
-	require "SemDesconto.php";
-	require "Imposto.php";
-	require "TemplateDeImpostoCondicional.php";
-	require "Orcamento.php";
-	require "CalculadoraDeImpostos.php";
-	require "ImpostoMuitoAlto.php";
-	require "ICMS.php";
-	require "ISS.php";
-	require "KCV.php";
-	require "ICCC.php";
-	require "EstadoDeUmOrcamento.php";
-	require "Aprovado.php";
-	require "EmAprovacao.php";
-	require "Reprovado.php";
-	require "Finalizado.php";
+	//error_reporting(0);
 
-	$reforma = new Orcamento(490);
+	require "ConnectionFactory.php";
+	
+	$factory = new ConnectionFactory();
 
-	echo $reforma->getValor()."<br />";
+	$conexao = $factory->getConnection();
 
-	$reforma->aplicaDesconto();
-
-	echo $reforma->getValor()."<br />";
-
-	$reforma->aprova();
-
-	$reforma->aplicaDesconto();
-
-	echo $reforma->getValor()."<br />";
-
-	$reforma->finaliza();
-
-	$reforma->aplicaDesconto();
-
-	echo $reforma->getValor()."<br />";
+	var_dump($conexao);
